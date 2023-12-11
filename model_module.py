aa517a54e30c076fc2426c2741e8d5300cf134e9
Gtnick1241 <taoguo@bu.edu>
2023-12-11 14:45:31 -0500

    model module

--- model_module.py
+++ model_module.py
@@ -1,13 +1,42 @@
 import torch
 import numpy as np
-import torch.nn as nn
-import torch.nn.functional as F
+# Data
+n_tracks = 5  # number of tracks
+n_pitches = 72  # number of pitches
+lowest_pitch = 24  # MIDI note number of the lowest pitch
+n_samples_per_song = 8  # number of samples to extract from each song in the datset
+n_measures = 4  # number of measures per sample
+beat_resolution = 4  # temporal resolution of a beat (in timestep)
+programs = [0, 0, 25, 33, 48]  # program number for each track
+is_drums = [True, False, False, False, False]  # drum indicator for each track
+track_names = ['Drums', 'Piano', 'Guitar', 'Bass', 'Strings']  # name of each track
+tempo = 100
+
+# Training
+batch_size = 16
+latent_dim = 128
+n_steps = 20000
+
+# Sampling
+sample_interval = 100  # interval to run the sampler (in step)
+n_samples = 4
+measure_resolution = 4 * beat_resolution
+tempo_array = np.full((4 * 4 * measure_resolution, 1), tempo)
+assert 24 % beat_resolution == 0, (
+    "beat_resolution must be a factor of 24 (the beat resolution used in "
+    "the source dataset)."
+)
+assert len(programs) == len(is_drums) and len(programs) == len(track_names), (
+    "Lengths of programs, is_drums and track_names must be the same."
+)
+# Assuming 'latent_dim' is the size of the noise vector for the generator
+latent_dim = 128 # Replace with the actual dimension of your latent space
 class Generator(torch.nn.Module):
     """A convolutional neural network (CNN) based generator. The generator takes
     as input a latent vector and outputs a fake sample."""
     def __init__(self):
         super().__init__()
-        self.transconv0 = GeneraterBlock(latent_dim, 256, (4, 1, 1), (4, 1, 1))
+        self.transconv0 = GeneraterBlock(128, 256, (4, 1, 1), (4, 1, 1))
         self.transconv1 = GeneraterBlock(256, 128, (1, 4, 1), (1, 4, 1))
         self.transconv2 = GeneraterBlock(128, 64, (1, 1, 4), (1, 1, 4))
         self.transconv3 = GeneraterBlock(64, 32, (1, 1, 3), (1, 1, 1))
